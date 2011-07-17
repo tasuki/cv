788c28e0b08c46c6ef6b5e5ed449f1f943aed258
Vit Brunner <vit.brunner@gmail.com>
2011-07-17 13:10:49 +0200

    use yaml for CV data; move examples to work directory

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+require_once('lib/spyc.php');
+$data = Spyc::YAMLLoad('cv.yaml');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title><?php echo $data['name'] ?></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="CV of Vit Brunner" />
+	<style type="text/css">
+		* { margin: 0px; padding: 0px; }
+		body { background-color: #FFF; color: #000; font-family: Candara, Verdana, sans-serif; font-size: 14px; }
+		div { clear: both; }
+		#wrap { width: 700px; padding: 20px; margin-left: 10px; }
+		.right { float: right; width: 500px; text-align: left; padding-top: 5px; }
+		.left { float: left; width: 200px; text-align: right; padding-top: 5px; }
+		.separator { padding: 0px 8px; }
+		h1 { letter-spacing: 1px; font-size: 25px; font-weight: normal; }
+		p { clear: both; padding: 5px 0px; }
+		.category { font-weight: bold; margin-top: 30px; padding: 0px; }
+		a:link { color: #000; }
+		a:visited { color: #000; }
+		a:hover { color: #69A; text-decoration: none; }
+	</style>
+</head>
+<body>
+
+<div id="wrap">
+<h1 class="right"><?php echo $data['name'] ?></h1>
+
+<?php
+
+function show_item($item, $description) {
+	echo "
+		<div>
+			<span class='left'>$item<span class='separator'>&raquo;</span></span>
+			<span class='right'>$description</span>
+			<div></div>
+		</div>";
+}
+
+foreach ($data['categories'] as $category => $items) {
+	echo "<div class='right category'>$category<div></div></div><br/>";
+
+	foreach ($items as $item => $desc) {
+		if (is_int($item)) {
+			$item = '';
+		}
+
+		if (is_array($desc)) {
+			show_item($item, array_shift($desc));
+			foreach ($desc as $part) {
+				show_item('', $part);
+			}
+		} else {
+			show_item($item, $desc);
+		}
+	}
+}
+?>
+
+</div>
+</body>
+</html>
